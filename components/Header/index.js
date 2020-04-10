d3b130e1d0ae3e0b504a89cb4a7d851b2b6c03ba
Ian McElroy <mcelroyian@gmail.com>
2020-04-10 10:22:30 -0500

    header component finished with dummy data

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,31 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    //helper
+    let make = (el) => document.createElement(el)
+
+    //create html elements
+    const header = make('div')
+    header.classList.add('header')
+
+    const date = make('span')
+    date.classList.add('date')
+    date.textContent = "SMARCH 28, 2019"
+    header.appendChild(date)
+
+    const h1 = make('h1')
+    h1.textContent = "Lambda Times"
+    header.appendChild(h1)
+
+    const temp = make('span')
+    temp.classList.add('temp')
+    temp.textContent = "98°"
+    header.appendChild(temp)
+
+    return header
+}
+
+// Cheeky one liner to invoke function and append to the header container
+document.querySelector('.header-container').appendChild(Header())
+
