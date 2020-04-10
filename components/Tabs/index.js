fe231d7e41ec058e48d976311ea7281186a34315
Ian McElroy <mcelroyian@gmail.com>
2020-04-10 10:39:39 -0500

    Tab component is pulling topics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+//Get data
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+        .then( res => {
+            const topics = res.data.topics
+
+            //loop over topics
+            topics.forEach( topic => {
+                // Create div.tab
+                const tab = document.createElement('div')
+                tab.classList.add('tab')
+
+                // set textContents to the topic name
+                tab.textContent = topic
+
+                //append to .topics
+                document.querySelector('.topics').appendChild(tab)
+            })
+            
+        })
+        .catch( err => {
+            console.log('Whoops, did not get data from Lambda times')
+            
+        })
+
